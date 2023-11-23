0d737430f61e85e6e8aa804af8d9b49eab276866
C1322309 Tiago Lira Batista <c1322309@interno.bb.com.br>
2023-11-23 14:50:22 -0300

    Arquivos 1-Hello Word,2-Values e 3-Values adicionados

---
+++ 2-Values/values.go
@@ -0,0 +1,32 @@
+// Go by Example: Values
+// Go has various value types including strings, integers, floats, booleans, etc.
+// Here are a few basic examples.
+
+// Strings, which can be added together with +.
+// Integers and floats.
+// Booleans, with boolean operators as you’d expect
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go" + "Lang")
+
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+// SAÍDA
+
+// $ go run values.go
+// golang
+// 1+1 = 2
+// 7.0/3.0 = 2.3333333333333335
+// false
+// true
+// false
