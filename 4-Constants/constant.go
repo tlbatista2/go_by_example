223e350f48e9a995ae3ae624efbe93ce5fb92e4a
C1322309 Tiago Lira Batista <c1322309@interno.bb.com.br>
2023-11-28 17:41:06 -0300

    Pasta Constants adicionada

---
+++ 4-Constants/constant.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	const n = 500000000
+
+	const d = 3e20 / n
+	println(d)
+
+	println(math.Sin(n))
+}
